68a7126d007b1109b7a068e7ad2539f0280baf6a
Willian Lameira <williansilvlameira@gmail.com>
2023-05-08 14:26:05 -0400

    Criação do código para calcular a pocentagem de caracteres digitados e mudança de cor personalida.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function mudaLimitador(limitadorId, contadorId) {
   counter.value = contador.textContent =
     "Caracteres Restantes: " + limitador.value + ".";
 }
-function contagemRegressiva(textareaId, contadorId, limitadorId) {
+function limitadorCaractere(textareaId, contadorId, limitadorId) {
   const limiteCaracteres = document.getElementById(limitadorId).value;
   const contador = document.getElementById(contadorId);
   const textarea = document.getElementById(textareaId);
@@ -13,14 +13,22 @@ function contagemRegressiva(textareaId, contadorId, limitadorId) {
   const caracteresDigitados = textarea.value.length;
   const caracteresRestantes = limiteCaracteres - caracteresDigitados;
 
-  if (caracteresRestantes > 0) {
+  if (caracteresRestantes >= 0) {
     contador.textContent = "Caracteres Restantes: " + caracteresRestantes + ".";
-    contador.style.color = "white";
-  } else if (caracteresRestantes == 0) {
-    contador.textContent = "Caracteres Restantes: " + caracteresRestantes + ".";
-    contador.style.color = "red";
+    let porcentagem = (caracteresDigitados / limiteCaracteres) * 100;
+    console.log(porcentagem);
+    if(porcentagem >= 0 && porcentagem <= 50){
+        contador.style.color = "#fff";
+        console.log('Entrou 1');
+    }else if( porcentagem > 51 && porcentagem < 99){
+        contador.style.color = "#ffa500";
+        console.log('Entrou 2');
+    }else if (porcentagem == 100){
+        contador.style.color = "#f00";
+        contador.textContent = "Limite excedido.";
+        console.log('Entrou 3');
+    }
   }
-
   if (caracteresDigitados >= limiteCaracteres) {
     textarea.value = textarea.value.substr(0, limiteCaracteres);
   }
